dddde7f263e110bbd9d608a67f2811be123b2c1d
tiwu5322 <45931898+tiwu5322@users.noreply.github.com>
2021-04-06 00:25:20 -0600

    Update setupKafka.sh
    Split installation script and launch script

--- kafka/setupKafka.sh
+++ kafka/setupKafka.sh
@@ -16,23 +16,3 @@ echo 'vm.swappiness=1' | sudo tee --append /etc.sysctl.conf
 wget https://mirrors.ocf.berkeley.edu/apache/kafka/2.7.0/kafka_2.13-2.7.0.tgz
 tar -xzf kafka_2.13-2.7.0.tgz
 rm kafka_2.13-2.7.0.tgz
-
-
-###########################################
-#                                         #
-#  run zookerper server and kafka server  #
-#                                         #
-###########################################
-# get internal ip
-KAFKA_DNS=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/hostname"   -H "Metadata-Flavor: Google")
-# get external ip
-#KAFKA_DNS=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-if [ "$PWD" = "/" ]; then
-	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" kafka_2.13-2.7.0/config/server.properties
-	sudo kafka_2.13-2.7.0/bin/zookeeper-server-start.sh kafka_2.13-2.7.0/config/zookeeper.properties &
-	sudo kafka_2.13-2.7.0/bin/kafka-server-start.sh kafka_2.13-2.7.0/config/server.properties &
-else
-	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" $(pwd)/kafka_2.13-2.7.0/config/server.properties
-	sudo $(pwd)/kafka_2.13-2.7.0/bin/zookeeper-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/zookeeper.properties &
-	sudo $(pwd)/kafka_2.13-2.7.0/bin/kafka-server-start.sh $(pwd)/kafka_2.13-2.7.0/config/server.properties &
-fi
