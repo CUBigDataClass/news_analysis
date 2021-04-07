be4ea209d20f6b58151af730cc1bcf383ef2c615
tiwu5322 <45931898+tiwu5322@users.noreply.github.com>
2021-04-07 14:47:51 -0600

    Update kafka-install.sh
    Added modifying config file scripts

--- kafka/kafka-install.sh
+++ kafka/kafka-install.sh
@@ -16,3 +16,13 @@ echo 'vm.swappiness=1' | sudo tee --append /etc.sysctl.conf
 wget https://mirrors.ocf.berkeley.edu/apache/kafka/2.7.0/kafka_2.13-2.7.0.tgz
 tar -xzf kafka_2.13-2.7.0.tgz
 rm kafka_2.13-2.7.0.tgz
+
+# get internal ip
+KAFKA_DNS=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/hostname"   -H "Metadata-Flavor: Google")
+# get external ip
+#KAFKA_DNS=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+if [ "$PWD" = "/" ]; then
+	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" kafka_2.13-2.7.0/config/server.properties
+else
+	sudo sed -i "s+#advertised.listeners=PLAINTEXT://your.host.name:9092+advertised.listeners=PLAINTEXT://$KAFKA_DNS:9092+g" $(pwd)/kafka_2.13-2.7.0/config/server.properties
+fi
