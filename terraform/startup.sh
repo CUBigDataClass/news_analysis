266698a8e35b782626749877192bb3a706a19a40
tblenahan <timothylenahan@Timothys-MacBook-Pro.local>
2021-04-19 22:03:58 -0600

    WIP: startup script

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -3,3 +3,13 @@ git clone https://github.com/CUBigDataClass/news_analysis.git
 cd news_analysis
 
 docker run docker/compose:1.29.1 up
+
+echo alias docker-compose="'"'docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.29.1'"'" >> ~/.bashrc
+
+source ~/.bashrc
+
+docker-compose up
